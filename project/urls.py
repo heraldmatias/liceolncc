b8c5f68d850a87a2da75fadf3fbdb6a2c184ef51
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-08-15 16:00:43 -0500

    init

---
+++ project/urls.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^chat/', include('jqchat.urls')),
+    (r'^grappelli/', include('grappelli.urls')),
+    (r'^$', 'home.views.index'),
+    (r'^home/', include('home.urls')),
+    (r'^ubigeo/', include('ubigeo.urls')),
+    (r'^dependencia/', include('dependencia.urls')),  
+    (r'^usuario/', include('usuario.urls')),
+    (r'^comunicacion/', include('comunicacion.urls')),
+    (r'^extras/', include('extras.urls')),
+	(r'^foro/', include('pybb.urls', namespace='pybb')),
+)
+
+from django.views.static import serve
+urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$',
+        serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'^static/(?P<path>.*)$',
+        serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^favicon\.ico$', 
+        'django.views.generic.simple.redirect_to', 
+        {'url': '/static/images/favicon.ico'}),
+)
+
